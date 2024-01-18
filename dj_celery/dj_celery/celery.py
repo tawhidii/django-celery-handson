ee9e7a57f96f450baa855de8ebd8874afd2b5a62
Khademul Bari <khademul.bari@v2.ltd>
2024-01-18 12:09:03 +0600

    [add] - task priority example

--- dj_celery/dj_celery/celery.py
+++ dj_celery/dj_celery/celery.py
@@ -5,9 +5,16 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dj_celery.settings")
 app = Celery("dj_celery")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
-app.conf.task_routes = {
-    "newapp.tasks.task1": {"queue": "queue1"},
-    "newapp.tasks.task2": {"queue": "queue2"},
+# app.conf.task_routes = {
+#     "newapp.tasks.task1": {"queue": "queue1"},
+#     "newapp.tasks.task2": {"queue": "queue2"},
+# }
+
+
+app.conf.broker_transport_options = {
+    "priority_steps": list(range(10)),
+    "sep": ":",
+    "queue_order_strategy": "priority",
 }
 
 app.autodiscover_tasks()
